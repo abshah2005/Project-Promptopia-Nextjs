cc24d72f7fb6b0de3fd5604ce3f84a783fb499f2
abshah2005 <abdullah03350904415@gmail.com>
2024-08-22 11:04:51 -0700

    typos fixed

--- utils/database.js
+++ utils/database.js
@@ -3,15 +3,35 @@ import mongoose from "mongoose";
 let isConnected=false;
 
 export const connectToDb=async ()=>{
-    mongoose.set("strictQuery",true);
-    if(isConnected){
-        console.log("Mongodb is already connected");
-        return;
-    }
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/Promptopia`)
-        isConnected=true;
-    } catch (error) {
+    // mongoose.set("strictQuery",true);
+    // if(isConnected){
+    //     console.log("Mongodb is already connected");
+    //     return;
+    // }
+    // try {
+    //     await mongoose.connect(`${process.env.MONGODB_URI}/Promptopia`)
+    //     isConnected=true;
+    // }
+    mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  }
+    
+    catch (error) {
         console.log(error);
     }
 }
